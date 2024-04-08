5ee6af9a4a5e014f8622bcdf857ff870655810fd
jlisita <lisita_julien@hotmail.fr>
2024-04-08 13:24:37 +0200

    add AllPokemonTypes component

--- src/AllPokemonTypes.jsx
+++ src/AllPokemonTypes.jsx
@@ -1,12 +1,26 @@
-
+import { useState } from 'react';
 
 const AllPokemonTypes = () => {
+
+    const[pokemons, setPokemon] = useState(null);
+
+    fetch("https://pokebuildapi.fr/api/v1/pokemon/limit/100")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+        setPokemon(data)
+    });
 
     return (
         <section>
             <h2>Les types de pokémons</h2>
+            {!pokemons ? <p>Types en cours de chargement</p> :
+            pokemons[0].apiResistances.map( (element) => {
+                return <p>{element.name}</p> 
+            })
+            }
         </section>
-
     );
 };
 
