fbde800ff9a5fbc1572efc23165e9cb247a1f6f8
jlisita <lisita_julien@hotmail.fr>
2024-04-08 11:20:44 +0200

    add header component

---
+++ src/Header.jsx
@@ -0,0 +1,22 @@
+import logo from './logo.png';
+
+const Header = () => {
+
+    return (
+    <header className='App-header'>
+            <img src={logo} alt="logo" width = "300"/>
+            <nav>
+                <ul>
+                    <li><a href="">100 premier pokémons</a></li>
+                    <li><a href="">Les types</a></li>
+                    <li><a href="">Pokémons feu</a></li>
+                    <li><a href="">Pokémons eau</a></li>
+                    <li><a href="">Pokémons plante</a></li>
+                </ul>
+            </nav>
+        </header>
+
+    );
+};
+
+export default Header;
